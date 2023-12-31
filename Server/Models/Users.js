319ef867ea375177666194afae1736936fcfcf2e
sk-py <77550660+sk-py@users.noreply.github.com>
2024-01-01 02:33:51 +0530

    All the files are uploaded

---
+++ Server/Models/Users.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const users = mongoose.model("users", userSchema);
+
+module.exports = users;
+// const mongoose = require("mongoose");
+// const UserSchema = new mongoose.Schema({
+//   fullName: {
+//     type: String,
+//     required: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// const users = mongoose.model("users", UserSchema);
+// module.exports = users;
